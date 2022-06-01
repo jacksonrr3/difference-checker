cba2fbda05394923b9b0e19d416e6c094d4b633c
e.zaborshchikov <e.zaborshchikov@yandex.ru>
2022-06-01 16:59:45 +0300

    complete binary file

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ const genDiff = (pathTofile1, pathTofile2) => {
   const normalPath2 = normalizePath(pathTofile2); 
 
   const fileObject1 = JSON.parse(fs.readFileSync(normalPath1));
-  const fileObject2 = JSON.parse(fs.readFileSync(normalPath2));  
-  console.log(fileObject1)
-  console.log(fileObject2);
+  const fileObject2 = JSON.parse(fs.readFileSync(normalPath2));
+
+  Object.keys(fileObject1);
+  Object.keys(fileObject2);
+
+  console.log(Object.keys(fileObject2))
+  console.log(Object.keys(fileObject1));
 
 };
 
